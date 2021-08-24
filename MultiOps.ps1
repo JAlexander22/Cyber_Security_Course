4262abe6fcf9a87017c8f321cff36d8a7dc41387
jalexander22 <jalexander@spartaglobal.com>
2021-08-24 10:29:34 +0100

    first commit

---
+++ MultiOps.ps1
@@ -0,0 +1,18 @@
+[int] $FIRST_NUM = Read-Host - Prompt 'Input a Number'
+[int] $SECOND_NUM = Read-Host - Prompt 'Input a second Number'
+$SUM = $FIRST_NUM + $SECOND_NUM
+$SUB = $FIRST_NUM - $SECOND_NUM
+$SMUL = $FIRST_NUM * $SECOND_NUM
+$DIV = $FIRST_NUM / $SECOND_NUM
+Write-Host $FIRST_NUM "+" $SECOND_NUM "=" $SUM
+Write-Host $SUM
+
+Write-Host $FIRST_NUM "-" $SECOND_NUM "=" $SUB
+Write-Host $SUB
+
+Write-Host $FIRST_NUM "*" $SECOND_NUM "=" $SMUL
+Write-Host $SMUL
+
+Write-Host $FIRST_NUM "/" $SECOND_NUM "=" $DIV
+Write-Host $DIV
+
