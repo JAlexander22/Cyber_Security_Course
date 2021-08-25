99153a31bdf69c82124496b5335033bc51239c9c
JAlexander22 <JAlexander@spartaglobal.com>
2021-08-25 13:36:04 +0100

    New commit

--- Results.ps1
+++ Results.ps1
@@ -1,19 +1,29 @@
+
+
+ 
 [int] $FIRST_NUM = Read-Host - Prompt 'Input a Number'
 [int] $SECOND_NUM = Read-Host - Prompt 'Input a second Number'
-$SUM = $FIRST_NUM + $SECOND_NUM
-$SUB = $FIRST_NUM - $SECOND_NUM
-$SMUL = $FIRST_NUM * $SECOND_NUM
-$DIV = $FIRST_NUM / $SECOND_NUM
-Write-Host $FIRST_NUM "+" $SECOND_NUM "=" $SUM
-Write-Host $SUM
 
-Write-Host $FIRST_NUM "-" $SECOND_NUM "=" $SUB
-Write-Host $SUB
+while ($FIRST_NUM -ge 0) {
+	if ($SECOND_NUM -eq 0){break}
+	Write-Host "Can't divide by 0" 
+	
+	$SUM = $FIRST_NUM + $SECOND_NUM
+	$SUB = $FIRST_NUM - $SECOND_NUM
+	$SMUL = $FIRST_NUM * $SECOND_NUM
+	$DIV = $FIRST_NUM / $SECOND_NUM
+	
 
-Write-Host $FIRST_NUM "*" $SECOND_NUM "=" $SMUL
-Write-Host $SMUL
 
-Write-Host $FIRST_NUM "/" $SECOND_NUM "=" $DIV
-Write-Host $DIV
+	Write-Host $FIRST_NUM "+" $SECOND_NUM "=" $SUM
+	Write-Host $SUM
 
-Write-Host "Testing 1,2,3"
+	Write-Host $FIRST_NUM "-" $SECOND_NUM "=" $SUB
+	Write-Host $SUB
+
+	Write-Host $FIRST_NUM "*" $SECOND_NUM "=" $SMUL
+	Write-Host $SMUL
+
+	Write-Host $FIRST_NUM "/" $SECOND_NUM "=" $DIV
+	Write-Host $DIV
+}
